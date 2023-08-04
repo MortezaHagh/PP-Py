b4ef39d8d16514001123c3b6f74db161a7485f53
Morteza Haghbeigi <morteza.haghbeigi@unitedrobots.co>
2023-08-04 14:46:40 +0200

    round distance euclidian

--- common/cal_distance.py
+++ common/cal_distance.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 def cal_distance(x1, y1, x2, y2, type):
     if type == 'euclidean':
-        dist = sqrt((x1-x2)**2+(y1-y2)**2)
+        dist = round(sqrt((x1-x2)**2+(y1-y2)**2), 5)
     elif type == 'manhattan':
         dist = abs(x1-x2) + abs(y1-y2)
     return dist
